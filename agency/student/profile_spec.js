43c8b46c5c5bb7fcf9113d355a537d4da3bdfc3e
Chowdhurian <manil.chowdhury@gmail.com>
2016-11-18 15:23:02 -0800

    🔥 skip test that will fail when not on local

--- agency/student/profile_spec.js
+++ agency/student/profile_spec.js
@@ -51,7 +51,7 @@ describe('the student profile page', () => {
     })
   })
 
-  describe('files (will fail when run not on local)', () => { // keep skipped when committing as uploading cant work for CircleCI
+  describe.skip('files (will fail when run not on local)', () => { // keep skipped when committing as uploading cant work for CircleCI
     beforeEach(() => {
       browser.get('/agency/en/504/student/listing/504')
       LoginPage.waitForLoader()
