c5f951e884ef0ebf26227b71d1d0437bb60849d9
Chowdhurian <manil.chowdhury@gmail.com>
2016-11-29 12:08:59 -0800

    📝 Remove note about failing test

--- agency/quotes/quotes_spec.js
+++ agency/quotes/quotes_spec.js
@@ -90,7 +90,7 @@ describe('the quotes page', () => {
         })
     })
 
-    it('(FAILING, MASTER, BUG FILED) adds an insurance addon with start plus end dates to a quote', () => {
+    it('adds an insurance addon with start plus end dates to a quote', () => {
       const quotesPage = new QuotesPage()
       const quotesListingPage = new quotesPage.QuotesListingPage()
       quotesListingPage.clickNewButton()
