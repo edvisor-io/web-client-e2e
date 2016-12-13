070cae70d5f6b27a79cd2377986ad2f31de5b89b
Chowdhurian <manil.chowdhury@gmail.com>
2016-12-12 20:28:20 -0800

    🎨 Make sSettings spec more resilient to lazy load

--- school/settings/campus/campusTab.pageObject.js
+++ school/settings/campus/campusTab.pageObject.js
@@ -1,7 +1,7 @@
 import CreateANewCampusModal from './createANewCampusModal.pageObject'
 import DuplicateCampusModal from './duplicateCampusModal.pageObject'
 
-export default class CampusTab {
+export default class CampusTabArea {
   constructor() {
     this.duplicateButton = $('div.flex-spread > div > button')
     this.selectCampusDropdown = element.all(by.css('div.selected_item')).first()
